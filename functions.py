ac4c29a43df59df64571192855043dbbaa5348d8
awoldt <48081351+awoldt@users.noreply.github.com>
2020-06-11 18:04:47 -0400

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
+inputsIndex = 0
+
 def createAcct():
     integer = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    currentInput = "First name: " #default value, user always asked for fname first
+    inputs = ("First name: ", "Last name: ", "Email: ")
 
     #returns true if user input contains integer, false if not 
     def containsNum(x):
@@ -13,20 +17,40 @@ def createAcct():
 
     #makes sure user input is in correct format
     def validateInput(x):
+        global inputsIndex
         error = True
         while(error == True):
             #if user doesn't enter anything
             if len(x) == 0:
                 print("\n>>Error: must enter value\n")
-                fname = input("First name: ")
-                x = fname
+                if(inputsIndex == 0):
+                    fname = input(inputs[inputsIndex])
+                    x = fname
+                elif(inputsIndex == 1):
+                    lname = input(inputs[inputsIndex])
+                    x = lname
+                elif(inputsIndex == 2):
+                    email = input(inputs[inputsIndex])
+                    x = email
             else:
                 isNum = containsNum(x)
                 if(isNum == True):
                     print("\n>>Error: cannot use integers in name field\n")
-                    fname = input("First name: ")
-                    x = fname
+                    if(inputsIndex == 0):
+                        fname = input(inputs[inputsIndex])
+                        x = fname
+                    elif(inputsIndex == 1):
+                        lname = input(inputs[inputsIndex])
+                        x = lname
+                    elif(inputsIndex == 2):
+                        email = input(inputs[inputsIndex])
+                        x = email
                 else:
+                    inputsIndex += 1
+                    filePath = open("/users/alex/documents/accts.txt", "a")
+                    filePath.write(x + "\n")
+                    filePath.close()
+
                     error = False
 
     loop = True
@@ -54,5 +78,3 @@ def accountAction(x):
         #DELETE
         elif(x == "delete"):
             print("delete")
-        else:
-            print("\n>>Error: invalid command '" + x + "'\n")
--- login.py
+++ login.py
@@ -4,6 +4,10 @@ print("\n")
 loop = True
 while(loop == True):
     user_request = input("Create, edit, or delete account? - ")
-    functions.accountAction(user_request) #decides what action user wants ex: (create,add,delete)
+    if user_request in ("create", "edit", "delete"):
+        functions.accountAction(user_request) #decides what action user wants ex: (create,add,delete)
+    else:
+        print("\n>>Error: unknown command '" + user_request + "'\n")
+        continue
     loop = False
 print("Goodbye!")
